c9d1234d80fa7c26fddf8b599532632e0633c3cc
Anthony N <foto.arn@gmail.com>
2020-12-29 18:37:34 -0800

    + Changed the whole layout

--- public/app.js
+++ public/app.js
@@ -22,17 +22,28 @@ tl.fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easInout })
     { opacity: 1, x: 0, ease: Power2.easInout },
     "-=1"
   )
+
   .fromTo(
-    headline,
-    1.2,
-    { y: "400%" },
-    { y: "-580%", ease: Power2.easInout },
+    ".links",
+    0.5,
+    { opacity: 0, x: 30 },
+    { opacity: 1, x: 0, ease: Power2.easInout },
     "-=1"
   );
-//   .fromTo(
-//     ".links",
-//     0.5,
-//     { opacity: 0, x: 30 },
-//     { opacity: 1, x: 0, ease: Power2.easInout },
-//     "-=1"
-//   );
+
+gsap.registerEffect({
+  name: "fade",
+  defaults: { duration: 2 }, //defaults get applied to the "config" object passed to the effect below
+  effect: (targets, config) => {
+    return gsap.to(targets, { duration: config.duration, opacity: 0 });
+  },
+});
+
+//now we can use it like this:
+//gsap.effects.fade(".box");
+
+document.querySelectorAll(".box").forEach(function (box) {
+  box.addEventListener("mouseenter", function () {
+    gsap.effects.fade(this);
+  });
+});
