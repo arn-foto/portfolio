2be81037ed61dfb81485eefbfcf1303f8cd9c23a
Anthony N <foto.arn@gmail.com>
2020-12-29 23:54:16 -0800

    + Finished contact container

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const hero = document.querySelector(".hero");
 const slider = document.querySelector(".slider");
 const logo = document.querySelector("#logo");
-const hamburger = document.querySelector(".hamburger");
+
 const headline = document.querySelector(".headline");
 
 const tl = new TimelineMax();
